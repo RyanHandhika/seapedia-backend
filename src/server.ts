ea2205a3ae61c9c9660a54878a215c41217b3cef
RyanHandhika <muhammadryanhandhikasusantorya@gmail.com>
2026-06-28 03:33:01 +0700

    vendor: setup for deploy on render

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import app from "./app.js";
 import http from "http";
 
 const server = http.createServer(app);
-const PORT = process.env.PORT || 4000;
+const PORT = Number(process.env.PORT) || 4000;
 
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
